e346ae2d97b37e4b5c40fb6e77b9e4295665c959
Andrew Luo <andrew.zhao.luo@gmail.com>
2020-04-05 18:40:24 -0700

    my solution

--- src/List/ArrayList.java
+++ src/List/ArrayList.java
@@ -1,7 +1,5 @@
 package List;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class ArrayList implements List{
     public static final int DEFAULT_ARRAY_SIZE = 10;
     private int length;
@@ -20,39 +18,78 @@ public class ArrayList implements List{
         this.length = 0;
     }
 
-    @Override
-    public int get(int i) {
-        // An example of throwing an exception.
-        if (i < 0 || i >= this.length)  {
+    // returns whether the index is valid 0 < i < length, if not, throws an exception
+    private void checkIndex(int i) {
+        if (i < 0 || i >= this.length) {
             throw new IndexOutOfBoundsException("Invalid index " + i);
         }
+    }
 
+    // resizes and copies over the underlying buffer with newBufferSize
+    private void resize(int newBufferSize) {
+        if (newBufferSize <= data.length) {
+            String errorMsg = "Requested buffer of " + newBufferSize + " but current buffer size is " + data.length;
+            throw new ArgumentException(errorMsg);
+        }
+
+        int[] newData = new int[newBufferSize];
+        for (int i = 0; i < length; i++) {
+            newData[i] = data[i]
+        }
+
+        data = newData;
+    }
+
+    @Override
+    public int get(int i) {
+        checkIndex(i);
         return data[i];
     }
 
     @Override
     public void insert(int i, int v) {
-        throw new NotImplementedException();
+        checkIndex(i);
+
+        // append to end of array, possibly resizing
+        addEnd(0);
+
+        // now this.length = 1 + oldLength, move up all elements starting from i
+        for (int j = i; j < this.length; j++) {
+            data[j] = data[j - 1];
+        }
+        data[j] = v;
     }
 
     @Override
     public int replace(int i, int v) {
-        throw new NotImplementedException();
+        checkIndex(i);
+        int old = data[i];
+        data[i] = v;
+        return old;
     }
 
     @Override
     public void addEnd(int v) {
-        throw new NotImplementedException();
+        if (length == data.length) {
+            resize(data.length * 2);
+        }
+
+        data[length - 1] = v;
     }
 
     @Override
     public int removeEnd() {
-        throw new NotImplementedException();
+        if (length == 0) {
+            throw new IllegalStateException("List is empty!");
+        }
+
+        int last = data[--length];
+        return last;
     }
 
     @Override
     public void clear() {
-        throw new NotImplementedException();
+        length = 0;
     }
 
     @Override
