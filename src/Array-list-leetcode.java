9302c7772e8d001ea4c75af80c604be124c9095f
jannetzhao <62923938+jannetzhao@users.noreply.github.com>
2020-04-09 20:09:44 -0700

    Update Array-list-leetcode.java

--- src/Array-list-leetcode.java
+++ src/Array-list-leetcode.java
@@ -24,3 +24,26 @@ class Solution {
         }
     }
 }
+
+// https://leetcode.com/problems/search-insert-position/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+            int low = 0;
+            int high = nums.length -1;
+            int mid = (high+low)/2;
+            
+            while(low <= high)
+            {
+                if(nums[mid] == target){
+                    return mid;
+                }
+                else if(nums[mid]>target){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            return low;
+    }
+}
