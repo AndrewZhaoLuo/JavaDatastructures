24662f4611e42f03b29391c6a9c878bc46d66c94
jannetzhao <62923938+jannetzhao@users.noreply.github.com>
2020-04-09 19:00:07 -0700

    Create Array-list-leetcode.java

---
+++ src/Array-list-leetcode.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxprofit = 0;
+        int tmp =0;
+        
+        for (int i = 1; i< prices.length; i++) {
+            if (prices[i] <= min){
+                min= prices[i];
+            }
+            else{
+                tmp = prices[i]-min;
+                if(tmp > maxprofit){
+                    maxprofit = tmp;
+                }
+            }
+        }
+        
+        return maxprofit;
+    }
+}
