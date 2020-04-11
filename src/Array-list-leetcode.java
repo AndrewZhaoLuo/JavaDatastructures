003be1c30f980f7347c27211685fa85b9a4330f1
jannetzhao <62923938+jannetzhao@users.noreply.github.com>
2020-04-10 19:55:29 -0700

    Update Array-list-leetcode.java

--- src/Array-list-leetcode.java
+++ src/Array-list-leetcode.java
@@ -49,3 +49,33 @@ class Solution {
             return low;
     }
 }
+
+// https://leetcode.com/problems/plus-one/
+public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        int carryover =0;
+        
+        digits[len-1] = digits[len-1] +1;
+            
+        for (int i = len-1;i>=0; i--){
+            digits[i] = digits[i] + carryover;
+            if(digits[i] == 10){
+                digits[i] = 0;        
+                carryover =1;
+            } 
+            else{   
+                return digits;
+                }
+            }
+        }
+        
+        if(carryover == 1){
+            int[] tmp = new int[len+1];
+            tmp[0]=1;
+            for(int j = 1; j<len+1; j++){
+                tmp[j]= digits[j-1];
+            }
+            return tmp;
+        }
+        return digits;
+    }
